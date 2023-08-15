18248c8a28adc63b6d23c20fc73ba74ec6441208
Ricky Kyte <137026079+rpk2301@users.noreply.github.com>
2023-08-15 11:56:47 -0400

    Building restful

---
+++ src/main/java/com/example/demo/BuildRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildRestApplication.class, args);
+	}
+
+}
