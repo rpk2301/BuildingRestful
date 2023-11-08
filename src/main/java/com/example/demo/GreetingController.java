3800207dbfd3a96f08c0aee45eaaa2322be73c70
Ricky <rpk2301@vt.edu>
2023-11-08 18:25:02 -0500

    I push code for Steve and Steve Only

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -15,7 +15,7 @@ public class GreetingController
 
 
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "address",defaultValue = "YOUR MOM")String name)
+    public Greeting greeting(@RequestParam(value = "address",defaultValue = "I am pushing code for Steve.")String name)
     {
         return new Greeting(counter.incrementAndGet(), String.format(template,name));
     }
